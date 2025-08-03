a01d2bb552a8ec1b1fbe53f9396746ab4778e376
BhagatVedant <tricksylife@gmail.com>
2025-08-03 19:45:44 +0530

    Remove unused test and world files; clean up project structure

--- test_game_over.gd
+++
@@ -1,6 +0,0 @@
-extends Node2D
-
-func _ready():
-	# Wait 2 seconds then show game over for testing
-	await get_tree().create_timer(2.0).timeout
-	SceneManager.show_game_over()
--- world.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-func _ready():
-	$LoopTimer.timeout.connect(_on_loop_timer_timeout)
-
-func _on_loop_timer_timeout():
-	get_tree().reload_current_scene()
